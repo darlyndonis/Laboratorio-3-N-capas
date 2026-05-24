486ee6c5c66f77d78c92bc13ebde6eb72fc6d0fe
darlyndonis <00083823@uca.edu.sv>
2026-05-23 22:09:31 -0600

    feat: implement specimen mapper for entity and DTO conversion

--- labo3/src/main/java/com/example/labo3/domain/mappers/SpecimenMapper.java
+++ labo3/src/main/java/com/example/labo3/domain/mappers/SpecimenMapper.java
@@ -1,5 +1,53 @@
 package com.example.labo3.domain.mappers;
 
+import com.example.labo3.domain.dto.request.CreateSpecimenRequest;
+import com.example.labo3.domain.dto.request.UpdateSpecimenRequest;
+import com.example.labo3.domain.dto.response.specimen.SpecimenResponse;
+import com.example.labo3.domain.entities.Specimen;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
+import java.util.UUID;
+
+@Component
 public class SpecimenMapper {
 
+    public Specimen toEntityCreate(CreateSpecimenRequest request) {
+
+        return Specimen.builder()
+                .name(request.getName())
+                .region(request.getRegion())
+                .dangerLevel(request.getDangerLevel())
+                .isFriendly(request.getIsFriendly())
+                .build();
+    }
+
+    public Specimen toEntityUpdate(
+            UpdateSpecimenRequest request,
+            UUID id
+    ) {
+
+        return Specimen.builder()
+                .id(id)
+                .name(request.getName())
+                .region(request.getRegion())
+                .dangerLevel(request.getDangerLevel())
+                .isFriendly(request.getIsFriendly())
+                .build();
+    }
+
+    public SpecimenResponse toDto(Specimen specimen) {
+
+        return SpecimenResponse.builder()
+                .id(specimen.getId())
+                .name(specimen.getName())
+                .region(specimen.getRegion())
+                .dangerLevel(specimen.getDangerLevel())
+                .isFriendly(specimen.getIsFriendly())
+                .build();
+    }
+
+    public Page<SpecimenResponse> toPageDto(Page<Specimen> specimens) {
+
+        return specimens.map(this::toDto);
+    }
 }
