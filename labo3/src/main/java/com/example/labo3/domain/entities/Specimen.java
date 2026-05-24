b136e746471a9b8406dd3003b46a136c81d7471f
darlyndonis <00083823@uca.edu.sv>
2026-05-23 22:03:22 -0600

    feat: add Specimen entity and JPA repository

--- labo3/src/main/java/com/example/labo3/domain/entities/Specimen.java
+++ labo3/src/main/java/com/example/labo3/domain/entities/Specimen.java
@@ -1,5 +1,31 @@
 package com.example.labo3.domain.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "specimen")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Specimen {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "region")
+    private String region;
+
+    @Column(name = "danger_level")
+    private Integer dangerLevel;
+
+    @Column(name = "is_friendly")
+    private Boolean isFriendly;
 }
+
--- labo3/src/main/java/com/example/labo3/domain/repositories/SpecimenRepository.java
+++ labo3/src/main/java/com/example/labo3/domain/repositories/SpecimenRepository.java
@@ -1,4 +1,9 @@
 package com.example.labo3.domain.repositories;
 
-public interface SpecimenRepository {
+import com.example.labo3.domain.entities.Specimen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
+
+public interface SpecimenRepository
+        extends JpaRepository<Specimen, UUID> {
 }
