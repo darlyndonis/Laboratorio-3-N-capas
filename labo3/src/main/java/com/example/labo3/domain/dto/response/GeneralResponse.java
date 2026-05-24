88936c2545d6e5d6a3301897639734e3a9c71968
darlyndonis <00083823@uca.edu.sv>
2026-05-23 22:17:34 -0600

    feat: implement GeneralResponse wrapper for API responses

--- labo3/src/main/java/com/example/labo3/domain/dto/response/GeneralResponse.java
+++ labo3/src/main/java/com/example/labo3/domain/dto/response/GeneralResponse.java
@@ -1,5 +1,17 @@
 package com.example.labo3.domain.dto.response;
 
-public class GeneralResponse {
+import lombok.*;
+import org.springframework.http.HttpStatus;
+import java.time.LocalDateTime;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GeneralResponse<T> {
+    private String message;
+    private HttpStatus status;
+    private T data;
+    private LocalDateTime timestamp;
+    private String path;
 }
